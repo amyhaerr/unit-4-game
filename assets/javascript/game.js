14b54a3aebb6debd9bbe539a4f67c2e8574a030a
Sangriale <senora.pelos@gmail.com>
2020-03-01 20:19:55 -0600

    made crystal buttons work with random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,47 +7,121 @@ $(document).ready(function () {
     var targetRanNumber = Math.floor(Math.random() * 102 + 19)
     console.log(targetRanNumber)
 
-    //shows random number on HTML 
-    $("#random-number").append(targetRanNumber);
 
+    // counters of wins/losses
     var counter = 0;
     console.log(counter)
     var wins = 0;
-        console.log(wins);
+    console.log(wins);
     var losses = 0;
-        console.log(losses);
-    
-      $('#wins-text').append(wins);
-        
-       $('#losses-text').append(losses);   
+    console.log(losses);
+
+    //shows random number on HTML 
+    $("#random-number").append(targetRanNumber).css("text-align", "center");
+
+    // shows wins/losses on HTML
+    $('#wins-text').append(wins);
+    $('#losses-text').append(losses);
+    $('#total-score').append(counter);
+
 
     // expand our array to include 4 random numbers
 
     var crystals = {
-        crystalOne: {
-            points: Math.floor(Math.random() * 12 + 1),
-            
+        crystal1: {
+            value: 0,
+
+
         },
-        crystalTwo: {
-            points: Math.floor(Math.random() * 12 + 1),
-            
+        crystal2: {
+            value: 0,
+
         },
-        crystalThree: {
-            points: Math.floor(Math.random() * 12 + 1),
-            
+        crystal3: {
+            value: 0,
+
         },
-        crystalFour: {
-            points: Math.floor(Math.random() * 12 + 1),
-            
+        crystal4: {
+            value: 0,
+
         },
     }
     console.log(crystals)
-    for (var i in crystals) {
-        var imgCrystalOne = $("<img>"); 
-        var imgCrystalTwo = $("<img>"); 
-        var imgCrystalThree = $("<img>"); 
-        var imgCrystalFour = $("<img>"); 
+    crystals.crystal1.value = Math.floor(Math.random() * 12 + 1);
+    crystals.crystal2.value = Math.floor(Math.random() * 12 + 1);
+    crystals.crystal3.value = Math.floor(Math.random() * 12 + 1);
+    crystals.crystal4.value = Math.floor(Math.random() * 12 + 1);
 
+    // Reset Game
+
+    function reset() {
+        var targetRanNumber = Math.floor(Math.random() * 102 + 19);
+        $("#random-number").text(targetRanNumber).css("text-align", "center");
+        var crystals = {
+            crystal1: {
+                value: 0,
+
+            },
+            crystal2: {
+                value: 0,
+
+            },
+            crystal3: {
+                value: 0,
+
+            },
+            crystal4: {
+                value: 0,
+
+            },
+
+        };
+        crystals.crystal1.value = Math.floor(Math.random() * 12 + 1);
+        crystals.crystal2.value = Math.floor(Math.random() * 12 + 1);
+        crystals.crystal3.value = Math.floor(Math.random() * 12 + 1);
+        crystals.crystal4.value = Math.floor(Math.random() * 12 + 1);
+        var counter = 0;
+        $("#total-score").append(counter).css("text-align", "center");
+    };
+
+
+    // Count hidden numbers under crystals
+    $("#white").on("click", function() {
+        counter = counter + crystals.crystal1.value;
+        $("#total-score").text(counter);
+    });
+
+    $("#blue").on("click", function() {
+        counter = counter + crystals.crystal2.value;
+        $("#total-score").text(counter);
+    });
+
+    $("#yellow").on("click", function() {
+        counter = counter + crystals.crystal3.value;
+        $("#total-score").text(counter);
+    });
+
+    $("#green").on("click", function() {
+        counter = counter + crystals.crystal4.value;
+        $("#total-score").text(counter);
+    });
+
+
+
+    // check if player won/lost
+    var checkScore = function () {
+        if (counter > targetRanNumber) {
+            alert("Please play again!");
+
+        }
     }
+
+    // for (var i in crystals) {
+    //     var imgCrystalOne = $("<img>"); 
+    //     var imgCrystalTwo = $("<img>"); 
+    //     var imgCrystalThree = $("<img>"); 
+    //     var imgCrystalFour = $("<img>"); 
+
+    // }
 
 });

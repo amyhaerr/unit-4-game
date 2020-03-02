f9ee323895f541aac575b04d010ef1a634703aa1
Sangriale <senora.pelos@gmail.com>
2020-03-02 13:33:19 -0600

    work on more jQuery and JavaScript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,126 +2,93 @@
 
 $(document).ready(function () {
 
-    // set variable to target random number
+    
+   // variables 
+   var counter = 0;
+   var wins = 0;
+   var losses = 0;
+   var targetRanNumber = 0;  
+   var crystals = {
+       crystal1: {
+           value: 0,
+       },
+       crystal2: {
+           value: 0,
+       },
+       crystal3: {
+           value: 0,
+       },
+       crystal4: {
+           value: 0,
+       },
+   }
 
-    var targetRanNumber = Math.floor(Math.random() * 102 + 19)
-    console.log(targetRanNumber)
+//    function to start 
 
-
-    // counters of wins/losses
-    var counter = 0;
-    console.log(counter)
-    var wins = 0;
-    console.log(wins);
-    var losses = 0;
-    console.log(losses);
-
-    //shows random number on HTML 
-    $("#random-number").append(targetRanNumber).css("text-align", "center");
-
-    // shows wins/losses on HTML
-    $('#wins-text').append(wins);
-    $('#losses-text').append(losses);
-    $('#total-score').append(counter);
-
-
-    // expand our array to include 4 random numbers
-
-    var crystals = {
-        crystal1: {
-            value: 0,
-
-
-        },
-        crystal2: {
-            value: 0,
-
-        },
-        crystal3: {
-            value: 0,
-
-        },
-        crystal4: {
-            value: 0,
-
-        },
-    }
-    console.log(crystals)
+function gameStart() {
+    targetRanNumber = Math.floor(Math.random() * 102 + 19);
     crystals.crystal1.value = Math.floor(Math.random() * 12 + 1);
     crystals.crystal2.value = Math.floor(Math.random() * 12 + 1);
     crystals.crystal3.value = Math.floor(Math.random() * 12 + 1);
     crystals.crystal4.value = Math.floor(Math.random() * 12 + 1);
 
-    // Reset Game
+    counter = 0;
 
-    function reset() {
-        var targetRanNumber = Math.floor(Math.random() * 102 + 19);
-        $("#random-number").text(targetRanNumber).css("text-align", "center");
-        var crystals = {
-            crystal1: {
-                value: 0,
+     //shows random number on HTML 
+     $("#random-number").text(targetRanNumber).css("text-align", "center");
 
-            },
-            crystal2: {
-                value: 0,
+     // shows wins/losses on HTML
+     $('#wins-text').text(wins).css("text-align", "center");
+     $('#losses-text').text(losses).css("text-align", "center");
+     $('#total-score').text(counter).css("text-align", "center");
+}
 
-            },
-            crystal3: {
-                value: 0,
+  
+  function scores() {
 
-            },
-            crystal4: {
-                value: 0,
+        if (counter > targetRanNumber) {
+            alert("You lost. Please play again!");
+        losses++;
 
-            },
+        $("losses-text").text(losses);
+        gameStart ();
 
-        };
-        crystals.crystal1.value = Math.floor(Math.random() * 12 + 1);
-        crystals.crystal2.value = Math.floor(Math.random() * 12 + 1);
-        crystals.crystal3.value = Math.floor(Math.random() * 12 + 1);
-        crystals.crystal4.value = Math.floor(Math.random() * 12 + 1);
-        var counter = 0;
-        $("#total-score").append(counter).css("text-align", "center");
-    };
+        }
+        else if (counter == targetRanNumber){
+            alert("You've won!");
+            wins++;
 
+            $("wins-text").text(wins);
+            gameStart ();
+        }
+    }
+  
 
+gameStart ();
     // Count hidden numbers under crystals
     $("#white").on("click", function() {
-        counter = counter + crystals.crystal1.value;
+         counter += crystals.crystal1.value;
         $("#total-score").text(counter);
+        scores();
     });
 
     $("#blue").on("click", function() {
-        counter = counter + crystals.crystal2.value;
+         counter += crystals.crystal2.value;
         $("#total-score").text(counter);
+        scores();
     });
 
     $("#yellow").on("click", function() {
-        counter = counter + crystals.crystal3.value;
+         counter += crystals.crystal3.value;
         $("#total-score").text(counter);
+        scores();
     });
 
     $("#green").on("click", function() {
-        counter = counter + crystals.crystal4.value;
+         counter += crystals.crystal4.value;
         $("#total-score").text(counter);
+        scores();
     });
 
-
-
-    // check if player won/lost
-    var checkScore = function () {
-        if (counter > targetRanNumber) {
-            alert("Please play again!");
-
-        }
-    }
-
-    // for (var i in crystals) {
-    //     var imgCrystalOne = $("<img>"); 
-    //     var imgCrystalTwo = $("<img>"); 
-    //     var imgCrystalThree = $("<img>"); 
-    //     var imgCrystalFour = $("<img>"); 
-
-    // }
 
 });
